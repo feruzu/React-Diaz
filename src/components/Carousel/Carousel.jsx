e718433f8c9ba48358552e5de86e835f2b5812f1
feruzu <fernanda23demarzo@gmail.com>
2022-09-30 22:46:37 -0300

    Edite Carousel y CartWidget

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -11,36 +11,29 @@ function Carrusel() {
     >
       <div className="carousel-inner">
         <div className="carousel-item active" data-bs-interval="3000">
-          <img src="./img/carousel-1.jpg" className="d-block w-100" alt="imagen carousel" />
+          <img
+            src="./img/carousel-1.jpg"
+            className="d-block w-100"
+            alt="imagen carousel 1"
+          />
         </div>
         <div className="carousel-item" data-bs-interval="3000">
-          <img src="./img/carousel-3.jpg" className="d-block w-100" alt="imagen carousel" />
+          <img
+            src="./img/carousel-3.jpg"
+            className="d-block w-100"
+            alt="imagen carousel 2"
+          />
         </div>
         <div className="carousel-item" data-bs-interval="3000">
-          <img src="./img/carousel-2.jpg" className="d-block w-100" alt="imagen carousel" />
+          <img
+            src="./img/carousel-2.jpg"
+            className="d-block w-100"
+            alt="imagen carousel 3"
+          />
         </div>
       </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
     </div>
   );
 }
-
 
 export default Carrusel;
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ function CartWidget(greeting) {
   return (
     <a href="/#">
       <i className="bi bi-bag"></i>
-      <span className="cantidad">0</span>
+      <span className="cantidad"></span>
     </a>
   );
 }
