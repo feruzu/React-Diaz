0aa187789b9096a777bb98933f50244c2bbce422
feruzu <fernanda23demarzo@gmail.com>
2022-10-26 23:17:38 -0300

    Ordene código

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,56 +1,49 @@
 import { useContext } from "react";
 import { cartContext } from "../../context/cartContext";
 import { Link } from "react-router-dom";
-import "./Cart.css"
+import "./Cart.css";
 import CheckoutForm from "../CheckoutForm/CheckoutForm";
 
+function Cart() {
+  const context = useContext(cartContext);
+  const { cart, removeItem, clear, total } = context;
 
+  if (cart.length === 0) {
+    return (
+      <div className="carrito-vacio">
+        <h1 className="carrito">Carrito</h1>
+        <h2 className="vacio">Vacío</h2>
+        <Link to="/">
+          <button className="volver-tienda">Volver a la Tienda</button>
+        </Link>
+      </div>
+    );
+  }
 
-function Cart(){
-
-    const context = useContext(cartContext);
-    const { cart, removeItem, clear, total } = context;
-
-
-
-if (cart.length === 0) {
   return (
-    <div className="carrito-vacio">
+    <div className="container-carrito">
       <h1 className="carrito">Carrito</h1>
-      <h2 className="vacio">Vacío</h2>
-      <Link to="/">
-        <button className="volver-tienda">Volver a la Tienda</button>
-      </Link>
+      {cart.map((item) => (
+        <div className="carrito-productos" key={item.id}>
+          <img src={item.img} alt="item img"></img>
+          <h3 className="item-titulo">{item.title}</h3>
+          <h2 className="item-cant">Cant. {item.count}</h2>
+          <h3 className="item-count">$ {item.price * item.count}</h3>
+          <button className="boton-borrar" onClick={() => removeItem(item.id)}>
+            X
+          </button>
+        </div>
+      ))}
+
+      <div className="container-total">
+        <span className="precio-total">Total: $ {total()} </span>
+        <button className="vaciar-carrito" onClick={() => clear()}>
+          Vaciar carrito
+        </button>
+      </div>
+      <CheckoutForm />
     </div>
   );
-}
-
-
-
-return (
-  <div className="container-carrito">
-    <h1 className="carrito">Carrito</h1>
-    {cart.map((item) => (
-      <div className="carrito-productos" key={item.id}>
-        <img src={item.img} alt="item img"></img>
-        <h3 className="item-titulo">{item.title}</h3>
-        <h2 className="item-cant">Cant. {item.count}</h2>
-        <h3 className="item-count">$ {item.price * item.count}</h3>
-        <button className="boton-borrar" onClick={() => removeItem(item.id)}>X</button>
-      </div>
-
-    ))}
-
-    <div className="container-total">
-    <span className="precio-total">Total: $ {total()} </span>
-    <button className="vaciar-carrito" onClick={() => clear()}>Vaciar carrito</button>
-    </div> 
-    <CheckoutForm/>
-    </div>
-
-
-
-);
 }
 
 export default Cart;
--- src/components/data-base/firestore.jsx
+++ src/components/data-base/firestore.jsx
@@ -1,6 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc, getDoc, query, where, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  query,
+  where,
+  addDoc,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,61 +18,54 @@ const firebaseConfig = {
   projectId: "react-serums",
   storageBucket: "react-serums.appspot.com",
   messagingSenderId: "83967177205",
-  appId: "1:83967177205:web:8fcc5216378b05a80930bc"
+  appId: "1:83967177205:web:8fcc5216378b05a80930bc",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
-
-// Items | Pedimos Documentos
-export async function getData(){
-  const miColeccion = collection( firestore, "productos");
+// Items
+export async function getData() {
+  const miColeccion = collection(firestore, "productos");
   let snapShot = await getDocs(miColeccion);
-    
-  let dataDocs =  snapShot.docs.map( documento => {
-    let docFormateado = { ...documento.data(), id: documento.id }
+
+  let dataDocs = snapShot.docs.map((documento) => {
+    let docFormateado = { ...documento.data(), id: documento.id };
     return docFormateado;
-});
-return dataDocs;
+  });
+  return dataDocs;
 }
 
-
-// Detalle de items | Pedimos Documento
-export async function getOneData(idParams){
-    
+// Detalle de items
+export async function getOneData(idParams) {
   const docRef = doc(firestore, "productos", idParams);
-  const docSnapShot =  await getDoc(docRef);
-  
-  return { ...docSnapShot.data(), id: docSnapShot.id }
+  const docSnapShot = await getDoc(docRef);
 
+  return { ...docSnapShot.data(), id: docSnapShot.id };
 }
-  
 
 // Categoria de Productos
-export async function getItemsByCategory(catParams){
+export async function getItemsByCategory(catParams) {
   const docsCollectionRef = collection(firestore, "productos");
   const q = query(docsCollectionRef, where("category", "==", catParams));
 
   const snapShot = await getDocs(q);
 
-  let dataDocs =  snapShot.docs.map( documento => {
-    let docFormateado = { ...documento.data(), id: documento.id }
+  let dataDocs = snapShot.docs.map((documento) => {
+    let docFormateado = { ...documento.data(), id: documento.id };
     return docFormateado;
-});
+  });
 
-return dataDocs;
+  return dataDocs;
 }
 
-// Manda a Firebase un objeto en una determinadad colección
-export async function createBuyOrder(orderData){
+// Ordenes de compra
+export async function createBuyOrder(orderData) {
   const miColeccion = collection(firestore, "orders");
   let respuesta = await addDoc(miColeccion, orderData);
- 
+
   return respuesta.id;
 }
 
-
-
-export default  firestore;
+export default firestore;
