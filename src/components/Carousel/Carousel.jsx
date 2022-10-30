3bb14c5decb434f4c87f192d6789d8fea391e8e8
feruzu <fernanda23demarzo@gmail.com>
2022-10-30 17:36:10 -0300

    Carousel indicadores

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -10,28 +10,6 @@ function Carrusel() {
       data-bs-interval="3000"
       data-bs-touch="true"
     >
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#myCarousel"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
           <img
@@ -54,6 +32,28 @@ function Carrusel() {
             alt="imagen carousel 3"
           />
         </div>
+        <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
       </div>
     </div>
   );
