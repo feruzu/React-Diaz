fcf36c10db0a2d02af772cbcfef7d196e5b8247c
feruzu <fernanda23demarzo@gmail.com>
2022-10-30 15:22:34 -0300

    Ordené código

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,9 +1,7 @@
 import "./Carousel.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function Carrusel() {
-
   return (
     <div
       id="myCarousel"
@@ -13,26 +11,43 @@ function Carrusel() {
       data-bs-touch="true"
     >
       <div className="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
       <div className="carousel-inner">
-        <div className="carousel-item active" >
+        <div className="carousel-item active">
           <img
             src="/./img/carousel-1.jpg"
             className="d-block w-100"
             alt="imagen carousel 1"
           />
         </div>
-        <div className="carousel-item" >
+        <div className="carousel-item">
           <img
             src="/./img/carousel-3.jpg"
             className="d-block w-100"
             alt="imagen carousel 2"
           />
         </div>
-        <div className="carousel-item" >
+        <div className="carousel-item">
           <img
             src="/./img/carousel-2.jpg"
             className="d-block w-100"
